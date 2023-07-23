674e5875fef294a619aeb9dc6630fb876636ba6f
Jesus <jesusvazquez2477@gmail.com>
2023-07-23 12:11:08 +0200

    Actualizo enlaces navbar

--- src/common/Navbar/Navbar.jsx
+++ src/common/Navbar/Navbar.jsx
@@ -1,32 +1,33 @@
-import React from 'react';
-import './Navbar.css'; 
+import React from "react";
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-return (
+  return (
     <nav className="navbar">
-    <div className="logo">MiLogo</div>
-    <ul className="nav-links">
-        <li>
-            <a href="#home">Inicio</a>
-        </li>
-        <li>
-            <a href="#servicios">Instalaciones</a>
-        </li>
-        <li>
-            <a href="#nosotros">Servicios</a>
-        </li>
-        <li>
-            <a href="#home">Alimentación</a>
-        </li>
-        <li>
-            <a href="#servicios">Citas online</a>
-        </li>
-        <li>
-            <a href="#nosotros">Nosotros</a>
-        </li>
-    </ul>
+      <div className="logo">MiLogo</div>
+      <ul className="nav-links">
+        <NavLink as={NavLink} to="/" exact="true" className="inicio">
+          <h5>Inicio</h5>
+        </NavLink>
+        <NavLink as={NavLink} to="/instalaciones" exact="true" className="inicio">
+          <h5>Instalaciones</h5>
+        </NavLink>
+        <NavLink as={NavLink} to="/nosotros" exact="true" className="inicio">
+          <h5>Servicios</h5>
+        </NavLink>
+        <NavLink as={NavLink} to="/nosotros" exact="true" className="inicio">
+          <h5>Alimentación</h5>
+        </NavLink>
+        <NavLink as={NavLink} to="/nosotros" exact="true" className="inicio">
+          <h5>Citas Online</h5>
+        </NavLink>
+        <NavLink as={NavLink} to="/nosotros" exact="true" className="inicio">
+          <h5>Sobre Nosotros</h5>
+        </NavLink>
+      </ul>
     </nav>
-);
+  );
 };
 
 export default Navbar;
