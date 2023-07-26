c9bb5749c89f4d8c0c3b23e2689c7c96b377e740
Jesus <jesusvazquez2477@gmail.com>
2023-07-26 02:13:11 +0200

    Implemento formulario para reservar cita online y actualizo estilos de vista de reservas, actualizo etiqueta reservas homePage

--- src/pages/AppointmentsPage/AppointmentsPage.jsx
+++ src/pages/AppointmentsPage/AppointmentsPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./AppointmentsPage.css";
+import icon1 from "../../assets/flechaHaciaAbajo.png";
 
 const AppointmentsPage = () => {
   return (
@@ -8,25 +9,73 @@ const AppointmentsPage = () => {
         <h2>¿Cómo reservar tu plaza?</h2>
         <div className="steps">
           <div className="step">
-            {/* <img src={icon1} alt="Icono 1" /> */}
             <p>Contacta con nosotros</p>
           </div>
           <div className="step">
-            {/* <img src={icon2} alt="Icono 2" /> */}
+            <img src={icon1} alt="Icono 2" className="icon1" />
             <p>Fija la fecha y la hora</p>
           </div>
           <div className="step">
-            {/* <img src={icon3} alt="Icono 3" /> */}
-            <p>Reserva tu plaza</p>
+            <img src={icon1} alt="Icono 3" className="icon1" />
+            <p className="textoPlaza">Reserva tu plaza</p>
           </div>
           <div className="step">
-            {/* <img src={icon4} alt="Icono 4" /> */}
+            <img src={icon1} alt="Icono 4" className="icon1" />
             <p>Recogemos a tu perro</p>
           </div>
         </div>
       </section>
       <section className="other-section">
-        {/* Otra sección con más información */}
+        <h2 className="textReserva">Reserva tu cita</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="name">Nombre completo:</label>
+            <input type="text" id="name" name="name" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Correo electrónico:</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="phone">Teléfono de contacto:</label>
+            <input type="tel" id="phone" name="phone" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="dogName">Nombre de tu perro:</label>
+            <input type="text" id="dogName" name="dogName" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="dogBreed">Raza de tu perro:</label>
+            <input type="text" id="dogBreed" name="dogBreed" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="appointmentDate">Fecha de la cita:</label>
+            <input
+              type="date"
+              id="appointmentDate"
+              name="appointmentDate"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="appointmentTime">Hora de la cita:</label>
+            <input
+              type="time"
+              id="appointmentTime"
+              name="appointmentTime"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="additionalInfo">Información adicional:</label>
+            <textarea
+              id="additionalInfo"
+              name="additionalInfo"
+              rows="4"
+            ></textarea>
+          </div>
+          <button type="submit">Reservar cita</button>
+        </form>
       </section>
     </div>
   );
